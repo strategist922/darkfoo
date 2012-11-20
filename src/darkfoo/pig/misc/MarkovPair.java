a3e56d2e3a6c03aae21fd95df00280d68f677be9
darkclown <darkclown@life-hack.org>
2012-11-20 20:35:42 +0900

    added copiwrite and license notice

--- src/darkfoo/pig/misc/MarkovPair.java
+++ src/darkfoo/pig/misc/MarkovPair.java
@@ -1,3 +1,19 @@
+/*
+Copyright (c) 2012 Johan Gustavsson <johan@life-hack.org>
+
+Darkfoo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Darkfoo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Darkfoo.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package darkfoo.pig.misc;
 import java.io.IOException;
 import java.util.Iterator;
