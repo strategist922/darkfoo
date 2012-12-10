42e50211d380199ae6d28cc664b63bc4439e4925
Darkclown <darkclown@life-hack.org>
2012-12-11 01:54:51 +0900

    added functunalities to search wordnet localy and a tool to generate parsable dictionary files

---
+++ src/Utils/WordWebGen/WordWebGenerate.java
@@ -0,0 +1,114 @@
+/*
+Copyright (c) 2012 Johan Gustavsson <johan@life-hack.org>
+
+Darkfoo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Darkfoo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Darkfoo.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import java.io.*;
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class WordWebGenerate{
+	private static Map<String, String> data;
+	public static void main(String[] args) {
+        data = new TreeMap<String, String>();
+        if(args.length < 2)
+            System.out.println("Have to specify source dict and type of words in dict eg. java WordWebGenerate data.noun noun");
+        else{
+            loadDict(args[0]);
+            buildDicts(args[1]);
+        }
+	}
+
+    private static void buildDicts(String dict){
+        try{
+            char last = 'a';
+            PrintWriter out = new PrintWriter(new File("dict/" + last + '.' + dict));
+            for (Map.Entry<String,String> entry : data.entrySet()) {
+                if(entry.getKey().charAt(0) != last){
+                    out.close();
+                    last = entry.getKey().charAt(0);
+                    out = new PrintWriter(new File("dict/" + last + '.' + dict));
+                }
+                out.println(entry.getKey() + "\t" + entry.getValue());
+            }
+            out.close();
+        }catch(IOException e){
+            //something
+        }
+    }
+
+	private static void loadDict(String dir){
+		InputStream inStream = null;
+        BufferedReader reader = null;
+        try{
+            inStream = new FileInputStream(dir);
+            reader = new BufferedReader(new InputStreamReader(inStream));
+
+            String line = null;
+            while((line = reader.readLine()) != null){
+                process(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                }
+            }
+
+            if (inStream != null) {
+                try {
+                    inStream.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+	}
+	private static void process(String line){
+		if((line.length() > 17) && (line.charAt(0) == '0')){
+            line = line.substring(17);
+            StringTokenizer st = new StringTokenizer(line, " ");
+            Set<String> work = new HashSet<String>();
+            while(st.hasMoreElements()){
+                String word = st.nextToken();
+
+                if(word.startsWith("00")){
+                    break;
+                }
+
+                if(word.length() > 2 && Character.isLetter(word.charAt(0)) && word.indexOf('_') == -1 
+                   && word.indexOf('-') == -1 && word.indexOf('(') == -1 
+                   && word.indexOf(')') == -1){
+                    work.add(word.toLowerCase());
+                }
+            }
+            if (work.size() >= 1) {
+                StringBuilder syns = new StringBuilder();
+                work = Collections.unmodifiableSet(work);
+                for (String word : work) {
+                    syns.append(word);
+                    syns.append(",");
+                }
+                syns.deleteCharAt(syns.length()-1);
+                for(String word : work)
+                    data.put(word, syns.toString());
+            }
+        }
+	}
+}
---
+++ src/darkfoo/pig/WordNet/Search.java
@@ -0,0 +1,89 @@
+/*
+Copyright (c) 2012 Johan Gustavsson <johan@life-hack.org>
+
+Darkfoo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Darkfoo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Darkfoo.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package darkfoo.pig.WordNet;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.BagFactory;
+
+public class Search extends EvalFunc<Tuple> {
+
+    private static final TupleFactory tupleFac = TupleFactory.getInstance();
+
+    public Tuple exec(Tuple input) throws IOException {
+        String instring = (String) input.get(0);
+        if(instring != null){
+            try {
+                String adj = null;
+                String adv = null;
+                String verb = null;
+                String noun = null;
+                InputStream inStream = Search.class.getResourceAsStream("/darkfoo/dict" + instring.charAt(0) + ".adj");
+                BufferedReader readbuffer = new BufferedReader(new InputStreamReader(inStream));
+                String strRead;
+                while ((strRead=readbuffer.readLine())!=null){
+                    String splitarray[] = strRead.split("\t");
+                    if(instring.equals(splitarray[0])){
+                        adj = splitarray[1];
+                    }
+                }
+                inStream = Search.class.getResourceAsStream("/darkfoo/dict" + instring.charAt(0) + ".adv");
+                readbuffer = new BufferedReader(new InputStreamReader(inStream));
+                while ((strRead=readbuffer.readLine())!=null){
+                    String splitarray[] = strRead.split("\t");
+                    if(instring.equals(splitarray[0])){
+                        adv = splitarray[1];
+                    }
+                }
+                inStream = Search.class.getResourceAsStream("/darkfoo/dict" + instring.charAt(0) + ".verb");
+                readbuffer = new BufferedReader(new InputStreamReader(inStream));
+                while ((strRead=readbuffer.readLine())!=null){
+                    String splitarray[] = strRead.split("\t");
+                    if(instring.equals(splitarray[0])){
+                        verb = splitarray[1];
+                    }
+                }
+                inStream = Search.class.getResourceAsStream("/darkfoo/dict" + instring.charAt(0) + ".noun");
+                readbuffer = new BufferedReader(new InputStreamReader(inStream));
+                while ((strRead=readbuffer.readLine())!=null){
+                    String splitarray[] = strRead.split("\t");
+                    if(instring.equals(splitarray[0])){
+                        noun = splitarray[1];
+                    }
+                }
+                Tuple res = tupleFac.newTuple(5);
+                res.set(0, instring);
+                res.set(1, adj);
+                res.set(2, adv);
+                res.set(3, verb);
+                res.set(4, noun);
+            
+                return res;
+            }catch(Exception e){
+                throw new RuntimeException("WordWeb Search error", e);
+            }
+        }
+        return null;
+    }
+}
